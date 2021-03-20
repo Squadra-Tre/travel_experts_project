de4f76a9e6815b8faf3be3b8394030a50bd7811d
Epiloguer <ssapergia99@gmail.com>
2021-03-20 13:56:41 -0600

    Some Changes

--- app.js
+++ app.js
@@ -3,18 +3,30 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
+//init app
 var app = express();
 
+//Middleware
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//load view engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+//Home Route
+const indexRouter = require('./routes/index');
 app.use('/', indexRouter);
+
+//Add Routes
+const usersRouter = require('./routes/users');
 app.use('/users', usersRouter);
+
+//Start server
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
 
 module.exports = app;
