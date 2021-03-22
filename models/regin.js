f44c9b3851d11ea37934ab34f73612c46b1d566f
Gitchub-lab <sanc8747@gmail.com>
2021-03-22 09:22:33 -0600

    here you go

--- models/regin.js
+++ models/regin.js
@@ -2,20 +2,26 @@
      Schema for Customers
 */
 
-const mongoose = require('mongoose');
+const mongoose = require('mongoose'), autoIncrement = require('mongoose-auto-increment');
 
 //mongoose.connect('mongodb://localhost:27017/newdb',
-mongoose.connect('mongodb+srv://travel-expert:travel1234@cluster0.orc02.mongodb.net/test?authSource=admin&replicaSet=atlas-71fnej-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true',
-    { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb+srv://travel-expert:travel1234@cluster0.orc02.mongodb.net/travelexperts_mongodb_json_collections?authSource=admin&replicaSet=atlas-71fnej-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true',
+    { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true });
+
+autoIncrement.initialize(mongoose);
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 
+//Schema for customers
+
 const regSchema = new mongoose.Schema({
+    _id: {
+        type: Number
+    },
+
     CustomerId: {
-        type: String,
-        trim: true,
-        required: [true, 'First Name cannot be empty']
+        type: Number
     },
 
     CustFirstName: {
@@ -76,26 +82,80 @@ const regSchema = new mongoose.Schema({
         required: [true, 'Email cannot be empty']
     },
 
-    AgentId: {
-        type: String,
-        trim: true,
-        required: [true, 'First Name cannot be empty']
-    },
-
     userid: {
         type: String,
         trim: true,
-        required: [true, 'First Name cannot be empty']
+        required: [true, 'User Name cannot be empty']
     },
 
     passwd: {
         type: String,
         trim: true,
-        required: [true, 'First Name cannot be empty']
+        required: [true, 'Password cannot be empty']
     },
 
+    versionKey: false,
+})
 
-});
+// autoincrement _id and CustomerId by 1
+regSchema.plugin(autoIncrement.plugin, { model: 'Customer', field: '_id', startAt: 149 });
+regSchema.plugin(autoIncrement.plugin, { model: 'Customer', field: 'CustomerId', startAt: 149 });
 
 //.Customer came from users.js const { Customer } = require("../models/regin");
 module.exports.Customer = mongoose.model('Customer', regSchema);
+
+
+// Schema for packages
+
+const packSchema = new mongoose.Schema({
+    PackageId: {
+        type: Number
+    }
+})
+
+
+module.exports.Package = mongoose.model('Package', packSchema);
+
+// Schema for booking
+
+const bookSchema = new mongoose.Schema({
+    _id: {
+        type: Number
+    },
+
+    BookingId: {
+        type: Number
+    },
+
+    BookingDate: {
+        type: Date,
+        required: false
+    },
+
+    BookingNo: {
+        type: String,
+        trim: true,
+    },
+
+    TravelerCount: {
+        type: Number,
+        required: [true, 'Traveller cannot be empty']
+    },
+
+    CustomerId: {
+        type: Number
+    },
+
+    TripTypeId: {
+        type: Number
+    },
+
+    PackageId: {
+        type: Number
+    }
+})
+
+bookSchema.plugin(autoIncrement.plugin, { model: 'Booking', field: '_id', startAt: 149 });
+//regSchema.plugin(autoIncrement.plugin, { model: 'Booking', field: 'CustomerId', startAt: 149 });
+
+module.exports.Booking = mongoose.model('Booking', bookSchema);
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,71 @@
 var express = require('express');
+const database = require('mime-db');
+//const { Console } = require('node:console');
 var router = express.Router();
 const { Customer } = require('../models/regin');
+const { Package } = require('../models/regin');
+const { Booking } = require('../models/regin');
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
+
+/* GET package page. */
+router.get("/packages", function (req, res, next) {
+  res.render("packg");
+});
+
+
+/* GET Register page. */
+router.get("/register", function (req, res, next) {
+  Package.findOne((err, package) => {
+    if (err) {
+      return res.send(500, err);
+    }
+    console.log('This is the package:', package);
+    res.render("customer", { package });
+
+  });
+});
+
+
+/* POST Register page. */
+router.post("/register", function (req, res, next) {
+  const myregist = new Customer(req.body);
+  myregist.save((err, result) => {
+    console.log('The data is saved in', result);
+    if (err) { //res.send(err);
+      console.log(err);
+    }
+  })
+  const getbooking = new Booking(req.body);
+  getbooking.save({
+    $currentDate: { BookingDate: true, "cancellation.date": { $type: "timestamp" } },
+  }, (err2, resb) => {
+    console.log('Booking is saved in', resb);
+    if (err2) {
+      console.log(err2);
+    }
+  })
+});
+
+
+/*res.render("thankyou", {
+  varbody: "div",
+  varbackimg: "div",
+  result
+});*/
+
+
+/* GET a Package */
+/*router.get("/:package", function (req, res, next) {
+  Package.findOne({ PackageId: req.params.package }, (err, result) => {
+    if (err) { //res.send(err);
+      console.log(err);
+      //console.log(result);
+    };
+  });
+})*/
 
 module.exports = router;
