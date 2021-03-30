8cbf22a1fd47be9bba60c6bfc593e562f4b30f93
Matt <80971715+mattbeez@users.noreply.github.com>
2021-03-29 19:01:21 -0600

    Added comments to my pages.

--- routes/delete.js
+++ routes/delete.js
@@ -1,5 +1,7 @@
 /*Matt Biesbroek - Team 3*/
 
+/*To Delete From the Travel Package Database*/
+
 var express = require('express');
 var router = express.Router();
 var deleteController= require('../controllers/delete-controller');
